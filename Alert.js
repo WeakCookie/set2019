aec559e7f387f5b18bf721d65e725133ef1233c1
Bùi Tuấn Anh <metwaroi31@gmail.com>
2019-04-23 10:04:51 +0700

    team2_errorHandler

---
+++ Alert.js
@@ -0,0 +1,4 @@
+class Alert {
+
+}
+module.exports.Alert = Alert
---
+++ errorHandler.js
@@ -0,0 +1,18 @@
+class errorHandler {
+    constructor (e) {
+        this.error = e
+    }
+    createErrorMessage () {
+        //type code here
+    }
+    specifyError () {
+        //type code here
+    }
+    renderError () {
+        //type code here
+    }
+    throwError () {
+        //type code here
+    }
+}
+module.exports.errorHandler = errorHandler
