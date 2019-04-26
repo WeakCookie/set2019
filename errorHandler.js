9954478e2727a91acd4dd70b2e0a3125683223b5
Bùi Tuấn Anh <metwaroi31@gmail.com>
2019-04-26 16:49:04 +0700

    final alert and errorhandler

--- errorHandler.js
+++ errorHandler.js
@@ -1,8 +1,84 @@
-let bb = require('./Alert')
+class Alert {
+    constructor(){
+        this.warn = "red"
+        this.warningColor = '#ff0000'
+        this.position = "top-right"
+        this.timeout = 2
+        this.isStacked = false
+        this.hasDisabledButton = false
+    }
+    setPosition(position){
+        switch (position) {
+            case "top-right" :
+                this.position = position
+                break;
+            case "top-left" :
+                this.position = position
+                break;
+            case "top-center" :
+                this.position = position
+                break;
+            case "bottom-right" :
+                this.position = position
+                break;
+            case "bottom-left" :
+                this.position = position
+                break;
+            case "bottom-center" :
+                this.position = position
+                break;
+            default :
+                break;
+        }
+    }
+    getPostion(){
+        let positionDescription = this.position.split('-')
+        let outPosition = ['10%', '10%']
+        if (positionDescription[1] == 'center') {
+            outPosition[1] = '50%'
+        }
+        return outPosition        
+    }
+    setTimeout(time){
+        if (typeof time != 'number') {
+            this.timeout = undefined
+        } else {
+            this.timeout = time
+        }
+    }
+    setStacked(canBeStacked){
+        if (typeof canBeStacked != 'boolean') {
+            this.isStacked = undefined
+        } else {
+            this.isStacked = canBeStacked
+        }
+    }
+    setWarnColor(color){
+        switch(color) {
+            case "red":
+                this.warn = 'red'
+                this.warningColor = '#ff0000'
+                break;
+            case "green":
+                this.warn = 'green'
+                this.warningColor = '#00ff00'
+                break;
+            case "yellow":
+                this.warn = 'yello'
+                this.warningColor = '#ffff00'
+                break;
 
+        }
+    }
+    setDisabledButton(){
+        this.hasDisabledButton = true
+    }
+    shutDisabledButton(){
+        this.hasDisabledButton = false
+    }
+}
 class errorHandler {
     constructor (e) {
-        alert('run')
         if (e instanceof Error) {
             this.error = e
         } else {
@@ -75,8 +151,7 @@ function aaa(){
         alert (a + b)
     } catch (e) {
         let checker = new errorHandler(e)
-        alert('run')
-        let ale = new alert()
+        let ale = new Alert()
         let aa = checker.renderError(ale)
         ele.appendChild(aa)
     }
