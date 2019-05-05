197b32fbe331737238306494ca51f6b6233bf8d8
Dinh Quoc Thuy <death.or.life.1999@gmail.com>
2019-05-05 09:15:31 +0700

    request-update

--- Request/script.js
+++ Request/script.js
@@ -1,20 +1,55 @@
+var listObject = []
 function request(url, options, callback)
 {
     var request = new XMLHttpRequest()
     request.onload = function (event) {
-    var data =JSON.parse(event.currentTarget.responseText)
-        callback(data)
+        if(this.status == "200") {
+            var data =JSON.parse(event.currentTarget.responseText)
+            callback(data)
+        }
     }
     request.open(options.method || "GET", url)
-    request.setRequestHeader("Access-Control-Allow-Origin","*")
     request.send()
 }
-function requestPeople(event) {
-    let getID = event.currentTarget.parentElement.id   
-    let temp = "https://swapi.co/api/" + getID
-    request(temp, {method: 'GET'},(data) => {
-        document.getElementById('height').innerHTML += data.height
-    })
-   
+function createList(selected,length) {
+    listObject = []
+    document.getElementById('out-put').innerHTML = ""
+    for(var i = 1; i <= length; i++) {
+        let url = "https://swapi.co/api/" + selected + '/' + i 
+        request(url,{method: 'GET'},(data) => {
+            listObject.push(data)
+            if (data.name == undefined) {
+            document.getElementById('out-put').innerHTML += data.title + '<br>'
+            }
+            else {
+                document.getElementById('out-put').innerHTML += data.name + '<br>'
+            }
+        })    
+    }
 }
+function selectOption () {
+
+    let selector = document.getElementById('select-box')
+    let selection = selector[selector.selectedIndex].value
+    if (selection == 'People') {  
+       createList("people",87)
+    }
+    
+    if (selection == 'Films') {
+       createList("films",7)     
+    }
+    
+    if (selection == 'Species') {
+        createList("species",35)
+    }
+    
+    if (selection == 'Planets') {
+        createList("planets",10)
+    }
+
+    if (selection == 'Vehicles') {
+        createList("vehicles",20)
+    }
+    
+  }
 
