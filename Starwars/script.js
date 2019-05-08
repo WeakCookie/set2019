a064685fdf46f2a0a94b073f3e619532135570c5
Bùi Tuấn Anh <metwaroi31@gmail.com>
2019-05-08 17:51:43 +0700

    checkboxes added

--- Starwars/script.js
+++ Starwars/script.js
@@ -13,23 +13,35 @@ return new Promise( function (resolve,reject) {
   }  
  })
 }
+let checkboxInfor = {}
 
 function display(url) {
   requestInfo(url)
   .then((data) => {
     list.innerHTML = ""
     for(let i = 0; i < data.results.length; i++) {
+      let objectDetail = document.createElement('div')
+      objectDetail.setAttribute('class', 'title-detail-tag')
+      let tickButton = createCheckBox({
+        'class': 'checkbox-to-combine',
+        'onclick': getCombineDetails
+      })
       if(data.results[i].name === undefined) {
-        list.innerHTML += '<button id="collapsible" onclick="detailsRequest('+ "'" + data.results[i].url +"'" +',event)"> <i class="fas fa-chevron-right"></i> '+ data.results[i].title + '</button>'
-        list.innerHTML += "<div id='details'></div>"
+        objectDetail.appendChild(tickButton)
+        objectDetail.innerHTML += '<button id="collapsible" onclick="detailsRequest('+ "'" + data.results[i].url +"'" +',event)"> <i class="fas fa-chevron-right"></i> '+ data.results[i].title + '</button>'
+        objectDetail.innerHTML += "<div id='details'></div>"
+        list.appendChild(objectDetail)
       }
       else {
-        list.innerHTML += '<button id="collapsible" onclick="detailsRequest('+ "'" + data.results[i].url +"'" +',event)"> <i class="fas fa-chevron-right"></i> '+ data.results[i].name + '</button>'
-        list.innerHTML += "<div id='details'></div>"
+        objectDetail.appendChild(tickButton)
+        objectDetail.innerHTML += '<button id="collapsible" onclick="detailsRequest('+ "'" + data.results[i].url +"'" +',event)"> <i class="fas fa-chevron-right"></i> '+ data.results[i].name + '</button>'
+        objectDetail.innerHTML += "<div id='details'></div>"
+        list.appendChild(objectDetail)
       }
     }
     if (data.previous !== null) {
-      list.innerHTML += '<button id="previous-page" onclick="display(' + "'" + data.previous + "'" +')">previous</button>'
+      objectDetail.innerHTML += '<button id="previous-page" onclick="display(' + "'" + data.previous + "'" +')">previous</button>'
+      list.appendChild(objectDetail)
     }
     if (data.next !== null) {
       list.innerHTML += '<button id="next-page" onclick="display(' + "'" + data.next + "'" +')">Next</button>'
@@ -64,20 +76,32 @@ function detailsRequest(url, event) {
 function displayDetails(data, currentElement) {
   detailsContent = currentElement.nextElementSibling
   detailsContent.innerHTML= ""
+  let objectDetail = document.createElement('div')
+  objectDetail.setAttribute('class', 'title-detail-tag')
+  let tickButton = createCheckBox({
+    'class': 'checkbox-to-combine',
+    'onclick': getCombineDetails
+  })
   for ( var key in data) {
     if(data[key].length === 0) {
-      detailsContent.innerHTML += "<p>"+ "<strong>" + key + "</strong>" + ": none</p>"
+      objectDetail.innerHTML += "<p>"+ "<strong>" + key + "</strong>" + ": none</p>"
+      detailsContent.appendChild(objectDetail)
     }
     else if(key !== 'url' && key !== 'episode_id' && data[key].indexOf('https') === 0 ) {
-      detailsContent.innerHTML += "<button id='collapsible' onclick = 'displaymore("+ '"' + data[key] + '"' +", event)'> <i class='fas fa-chevron-right'></i> " + "<strong>" + key + "</strong>" + "</button>"
-      detailsContent.innerHTML += "<div id='details'></div>"
+      objectDetail.appendChild(tickButton)
+      objectDetail.innerHTML += "<button id='collapsible' onclick = 'displaymore("+ '"' + data[key] + '"' +", event)'> <i class='fas fa-chevron-right'></i> " + "<strong>" + key + "</strong>" + "</button>" + "</input>"
+      objectDetail.innerHTML += "<div id='details'></div>"
+      detailsContent.appendChild(objectDetail)
     }
     else if (typeof data[key] === "object") {
-      detailsContent.innerHTML += "<button id='collapsible' onclick = 'displaymore("+ '"' + data[key] + '"' +", event)'> <i class='fas fa-chevron-right'></i> " + "<strong>" + key + "</strong>" + "</button>"
-      detailsContent.innerHTML += "<div id='details'></div>"
+      objectDetail.appendChild(tickButton)
+      objectDetail.innerHTML += "<button id='collapsible' onclick = 'displaymore("+ '"' + data[key] + '"' +", event)'> <i class='fas fa-chevron-right'></i> " + "<strong>" + key + "</strong>" + "</button>" + "</input>"
+      objectDetail.innerHTML += "<div id='details'></div>"
+      detailsContent.appendChild(objectDetail)
     } 
     else {
-      detailsContent.innerHTML += "<p>"+ "<strong>" + key + "</strong>" + ": " + data[key] +"</p>"
+      objectDetail.innerHTML += "<p>"+ "<strong>" + key + "</strong>" + ": " + data[key] +"</p>"
+      detailsContent.appendChild(objectDetail)
     }
   }
   collapseAnimation (detailsContent)
@@ -144,4 +168,37 @@ function collapseAnimation (detailsContent) {
   } else {
     detailsContent.style.maxHeight = detailsContent.scrollHeight + "px"
   } 
+}
+function createCheckBox(options){
+  let tickButton = document.createElement('input')
+  tickButton.setAttribute('type', 'checkbox')
+  for (var atrribute in options) {
+    let eventChecker = atrribute.indexOf('on')
+    if (eventChecker == -1){
+      tickButton.setAttribute(atrribute, options[atrribute])
+    } else {
+      let eventToBeAdded = atrribute.slice(2, atrribute.length)
+      tickButton.addEventListener(eventToBeAdded, options[atrribute])
+    }
+  }
+  return tickButton
+}
+function getCombineDetails(event) {
+  alert('run')
+  let toBeChecked = event.currentTarget
+  if(toBeChecked.checked) {
+    alert('testing')
+  } else{
+    
+  }
+}
+function combineSelectedItems() {
+  let itemsToCombine = document.getElementsByClassName('title-detail-tag')
+  let numberOfItems = itemsToCombine.length
+ for (var i = 0; i < numberOfItems;i++) {
+    let checkbox = itemsToCombine[i].children[0]
+    if(checkbox.checked) {
+      alert(i)
+    }
+  }
 }
