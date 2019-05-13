43b8a5002ad34da94faa574084b9b31c8eb99150
Nguyên <nguyenln2204@gmail.com>
2019-05-13 20:15:51 +0700

    Review

---
+++ api/AddTask.js
@@ -0,0 +1,32 @@
+function add() {
+    var check = document.getElementById('header-taskname')
+    if (check.value.trim() != '') {
+        var add = document.getElementById('header-taskname')
+        var item = document.createElement('li')
+        var currentValue = add.value.trim()
+        item.innerHTML += '<label><input type="checkbox" onclick="checkTask(event)"/>' + currentValue + '</label>'
+        item.innerHTML += '<button class="delete-button" onclick="deleteItem(event)">Delete</button>'
+        item.innerHTML += '<button class="edit-button" onclick="editTaskName(event)">Edit</button>'
+        taskList.append(item)
+        console.log(item)
+        requestAdd(currentValue)
+    }
+    else {
+        console.log('failed')
+    }
+    
+}
+
+function requestAdd (currentValue, url) {
+    let requestAdd = new XMLHttpRequest()
+    requestAdd.open('POST', url)
+    requestAdd.send(JSON.stringify(currentValue))
+    requestAdd.onload = function() {
+        if (this.status == 200) {
+            var data = document.getElementById('header-taskname').value
+            console.log('Add successfully!')
+            add(data)
+            data = ''
+        }
+    }
+}
