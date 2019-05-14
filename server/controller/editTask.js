92a4349f61962aad5d3bcd9eaa780cfa38e0edf2
Vu Thien <St.violent@gmail.com>
2019-05-14 17:52:08 +0700

    Update

--- server/controller/editTask.js
+++ server/controller/editTask.js
@@ -1,11 +1,14 @@
-if(req.url == '/api/editTask') {
-    getStream((data) => {
-        for(let i = 0; i < taskList.length; i++ ) {
-            if(taskList[i].taskName == data.originalName) {
-                taskList[i].taskName = data.replaceName
-                res.end('success')
+function editTask(event){
+    if(req.url == '/api/editTask') {
+        getStream((data) => {
+            for(let i = 0; i < taskList.length; i++ ) {
+                if(taskList[i].taskName == data.originalName) {
+                    taskList[i].taskName = data.replaceName
+                    res.end('success')
+                }
             }
-        }
-        res.end('task not found')
-    })
+            res.end('task not found')
+        })
+    }
 }
+module.exports = editTask
